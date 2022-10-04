47512b71e2ad74473ed9807a718bb58d94db9287
Martin <jullum@nr.no>
2022-10-04 09:52:59 +0200

    fix checks

--- R/mcceR-package.R
+++ R/mcceR-package.R
@@ -1,9 +1,9 @@
 #' @importFrom data.table data.table fread fwrite setnames := year month
-#' uniqueN setkey as.data.table copy between is.data.table setcolorder rbindlist
+#' uniqueN setkey set setDT as.data.table copy between is.data.table setcolorder rbindlist
 #'
 #' @importFrom utils head tail
 #'
-#' @importFrom stats predict
+#' @importFrom stats predict as.formula
 #'
 #' @keywords internal
 "_PACKAGE"
--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,14 @@
       ".N",
       ".I",
       ".GRP",
-      ".SD"
+      ".SD",
+      "id_explain",
+      "measure_euclidean",
+      "measure_manhattan",
+      "measure_validation",
+      "measure_sparsity",
+      "row_id",
+      "counterfactual_rank"
     )
   )
   invisible()
