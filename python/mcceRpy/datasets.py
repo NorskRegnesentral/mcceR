ce7324d88410803b68f6c2ad5b970413eb5db8e3
Martin <jullum@nr.no>
2023-06-23 14:38:41 +0200

    modify Python lm example

--- python/mcceRpy/datasets.py
+++ python/mcceRpy/datasets.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.datasets import fetch_california_housing, load_iris, load_boston
+from sklearn.datasets import fetch_california_housing, load_iris
 from sklearn.model_selection import train_test_split
 
 
@@ -17,19 +17,5 @@ def load_binary_iris():
   dfx = pd.DataFrame(bcancer.data, columns=bcancer.feature_names).iloc[bcancer.target<2] # Turning it into a binary classification problem
   dfy = pd.DataFrame({'target': bcancer.target}).iloc[bcancer.target<2] # Turning it into a binary classification problem
   dfx_train, dfx_test, dfy_train, dfy_test = train_test_split(dfx, dfy, test_size=5, random_state=42)
-  return dfx_train, dfx_test, dfy_train, dfy_test
-
-def load_boston2():
-  boston = load_boston()
-  dfx0 = pd.DataFrame(boston.data, columns=boston.feature_names)
-  dfx = dfx0.loc[:,["LSTAT","RM","DIS","INDUS"]]
-  dfy = pd.DataFrame({'MEDV': boston.target})
-
-  dfx_test = dfx.loc[0:6,]
-  dfx_train = dfx.loc[7:507,]
-
-  dfy_test = dfy.loc[0:6,]
-  dfy_train = dfy.loc[7:507,]
-
   return dfx_train, dfx_test, dfy_train, dfy_test
 
